b60b09541bde672bade6bb7f2e4f59f27b7afb73
bulba <bulba@10.81.4.190>
2020-02-04 16:24:41 -0500

    nothing

--- src/githubproject/GitHubProject.java
+++ src/githubproject/GitHubProject.java
@@ -16,6 +16,10 @@ public class GitHubProject {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        System.out.println("Test");
+        
+        
     }
     
 }
